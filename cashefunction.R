2329e292e90830df50f7018b0c5fb3152c44c014
Radhakant Tiwadi <therktiwadi@gmail.com>
2021-04-15 12:51:07 +0530

    cimments added

--- cashefunction.R
+++ cashefunction.R
@@ -2,25 +2,27 @@ makeCachmatrix<-function(x=matrix()){
   int<-NULL
   set<-function(y){
     x<<-y
-    inv<<-NULL
+    int<<-NULL
   }
+  
   get<-function(){x}
   setInverse<-function(inverse){inv<<-inverse}
   getInverse<-function() {inv}
   list(set=set,get=get,setInverse=setInverse,getInverse=getInverse)
-}
+  }
 
 #function to invert the matrix
 
 cachesolve<-function(x,...){
   inv<-x$getInverse()
-  if(!is.null(inv)){
+  
+  if(!is.null(inv))
+    {
     message("gettting cached data")
     return(inv)
-  }
-  
+    }
   mat<-x$get()
-  inv<-solve(mat,...)
+  inv<-solve(mat, ...)
   x$setInverse(inv)
   inv
 }
